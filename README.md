b126643acab918ea54f021858deeecb08505172d
junipertcy <junipertcy@gmail.com>
2015-06-21 18:13:26 +0800

    setup databases

--- README.md
+++
@@ -1,4 +0,0 @@
-Express-mongoose Template
-=====================
-
-[http://zihua.li/2014/05/good-practices-to-structure-an-express-app/](http://zihua.li/2014/05/good-practices-to-structure-an-express-app/)
--- models/index.js
+++ models/index.js
@@ -1,17 +1,25 @@
 var models = require('node-require-directory')(__dirname);
 var mongoose = require('mongoose');
+var lingo = require('lingo');
 var config = require('config');
 
-mongoose.connect('mongodb://' + config.mongodb.host + '/' + config.mongodb.database);
+var DB = mongoose.connect('mongodb://' + config.mongodb.host + '/' + config.mongodb.database);
+var LogDB = mongoose.createConnection('mongodb://' + config.logMongodb.host + '/' + config.logMongodb.database);
+
+var self = module.exports = {};
+
 
 Object.keys(models).forEach(function(key) {
-  if (key === 'index') return;
-  var modelName = capitaliseFirstLetter(key);
-  global[modelName] = mongoose.model(modelName, models[key]);
+  if (key !== 'index') {
+    var modelName = lingo.capitalize(key);
+    if (modelName === 'Log') {
+      self[modelName] = LogDB.model(modelName, models[key]);
+    } else {
+      self[modelName] = DB.model(modelName, models[key]);
+    }
+  }
 });
 
-global.mongoose = mongoose;
+self.DB = DB;
 
-function capitaliseFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+global.mongoose = mongoose;
