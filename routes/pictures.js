a5a80755ca7e40af94df43c2b210cf567d8754fd
junipertcy <junipertcy@gmail.com>
2015-07-28 22:49:32 +0800

    fix conflicts

--- routes/pictures.js
+++ routes/pictures.js
@@ -1,44 +1,19 @@
 /* Input: (lng, lat)
  * Output: [pic urls]
  */
-
-<<<<<<< HEAD
-//必須要檢查輸入，若不正確要報錯，避免重啟
-=======
-
->>>>>>> e2629959c92c1230000fb307454a3f74d5786d37
 var express = require('express');
 
 var app = module.exports = express();
 
 app.get('/', function(req, res) {
-<<<<<<< HEAD
-  console.log('Hello, you have entered the pictures API!');
-=======
-  console.log('Hello, here\'s the pictures API!');
-
->>>>>>> e2629959c92c1230000fb307454a3f74d5786d37
-
   if (!req.query.lat || !req.query.lng){
     return res.status(400).json({
       msg: 'latlng is required'
     });
   }
-<<<<<<< HEAD
 
-  var errors = [];
-  req.onValidationError(function(msg){
-    errors.push(msg);
-  });
-
-  req.checkBody('lat', {errcode: '', errmsg: ''}).notEmpty().
-
-=======
->>>>>>> e2629959c92c1230000fb307454a3f74d5786d37
   var lat = req.query.lat;
   var lng = req.query.lng;
-
-  console.log(typeof lng, typeof lat);
 
   console.log('Before finding pictures....');
 
