0723571046acd4005d7d6e8ea764ef3318decc1c
junipertcy <junipertcy@gmail.com>
2015-08-05 19:16:54 +0800

    minimal server files

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var routes = require('require-directory')(module);
+var fs = require('fs');
+var html = fs.readFileSync('view/index.html');
 
 module.exports = function(app) {
   Object.keys(routes).forEach(function(key) {
@@ -9,6 +11,13 @@ module.exports = function(app) {
 
   app.get('/', function(req, res) {
     res.json({ 'Urban Walkability System Version': '0.0.1' });
+  });
+
+  //acmmm-2015: NYC Grand Challenge
+  app.get('/noise', function(req, res) {
+    res.writeHead(200);
+    res.write(html);
+    res.end();
   });
 
 };
