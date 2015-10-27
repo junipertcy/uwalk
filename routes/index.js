f61fe4a5ad03f7fb721e3819d9967bc36ae4f39d
junipertcy <junipertcy@gmail.com>
2015-10-28 04:22:48 +1000

    add url moving message; debug

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var routes = require('require-directory')(module);
-var urllib = require('urllib');
 
 module.exports = function(app) {
   Object.keys(routes).forEach(function(key) {
@@ -10,18 +9,5 @@ module.exports = function(app) {
 
   app.get('/', function(req, res) {
     res.json({ 'Urban Walkability System Version': '0.0.1' });
-  });
-
-  //acmmm-2015: NYC Grand Challenge
-  app.get('/noise', function(req, res) {
-
-    var url = 'http://junipertcy.info/acmmm-2015';
-    urllib.request(url, function(err, data, response){
-      console.log(err);
-      console.log(data);
-      console.log(response);
-      res.write(data);
-      res.end();
-    });
   });
 };
--- routes/noise.js
+++ routes/noise.js
@@ -4,6 +4,6 @@ var app = module.exports = express();
 app.get('/', function(req, res) {
   res.status(200).json({
     code: 0,
-    msg: 'Sorry, our demo app has been temporarily moved to http://junipertcy.info/noise Please visit then.  --TC'
+    msg: 'Sorry, our demo app has been temporarily moved to \"http://junipertcy.info/noise\". Please visit there then.  --TC'
   });
 });
